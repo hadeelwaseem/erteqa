f7eb5e8e5f640e8d69e5d9d54307ea044b4c6dac
Anas Alzahabi <anas.alzahabe.2004@gmail.com>
2026-01-10 02:39:49 +0300

    cleaning shared_Prefrences

--- lib/core/utils/app_bloc_observer.dart
+++ lib/core/utils/app_bloc_observer.dart
@@ -1,44 +1,47 @@
-// // ignore_for_file: avoid_print
+// ignore_for_file: avoid_print
 
-// import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-// class AppBlocObserver extends BlocObserver {
-//   @override
-//   void onEvent(Bloc bloc, Object? event) {
-//     print('${DateTime.now()}: ${bloc.runtimeType} dispatching event $event');
-//     super.onEvent(bloc, event);
-//   }
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    print('${DateTime.now()}: ${bloc.runtimeType} dispatching event $event');
+    super.onEvent(bloc, event);
+  }
 
-//   @override
-//   void onTransition(Bloc bloc, Transition transition) {
-//     print(
-//         '${DateTime.now()}: ${bloc.runtimeType} transitioning from ${transition.currentState} to ${transition.nextState} due to event ${transition.event}');
-//     super.onTransition(bloc, transition);
-//   }
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(
+      '${DateTime.now()}: ${bloc.runtimeType} transitioning from ${transition.currentState} to ${transition.nextState} due to event ${transition.event}',
+    );
+    super.onTransition(bloc, transition);
+  }
 
-//   @override
-//   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-//     print(
-//         '${DateTime.now()}: ${bloc.runtimeType} encountered an error $error with stacktrace $stackTrace');
-//     super.onError(bloc, error, stackTrace);
-//   }
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print(
+      '${DateTime.now()}: ${bloc.runtimeType} encountered an error $error with stacktrace $stackTrace',
+    );
+    super.onError(bloc, error, stackTrace);
+  }
 
-//   @override
-//   void onChange(BlocBase bloc, Change change) {
-//     print(
-//         '${DateTime.now()}: ${bloc.runtimeType} state changed from ${change.currentState} to ${change.nextState}');
-//     super.onChange(bloc, change);
-//   }
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    print(
+      '${DateTime.now()}: ${bloc.runtimeType} state changed from ${change.currentState} to ${change.nextState}',
+    );
+    super.onChange(bloc, change);
+  }
 
-//   @override
-//   void onClose(BlocBase bloc) {
-//     print('${DateTime.now()}: ${bloc.runtimeType} is being closed');
-//     super.onClose(bloc);
-//   }
+  @override
+  void onClose(BlocBase bloc) {
+    print('${DateTime.now()}: ${bloc.runtimeType} is being closed');
+    super.onClose(bloc);
+  }
 
-//   @override
-//   void onCreate(BlocBase bloc) {
-//     print('${DateTime.now()}: ${bloc.runtimeType} is being created');
-//     super.onCreate(bloc);
-//   }
-// }
+  @override
+  void onCreate(BlocBase bloc) {
+    print('${DateTime.now()}: ${bloc.runtimeType} is being created');
+    super.onCreate(bloc);
+  }
+}
--- lib/core/utils/size_config.dart
+++ lib/core/utils/size_config.dart
@@ -1,13 +1,13 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class SizeConfig {
-//   static const double desktop = 1200;
-//   static const double tablet = 800;
+class SizeConfig {
+  static const double desktop = 1200;
+  static const double tablet = 800;
 
-//   static late double width, height;
+  static late double width, height;
 
-//   static init(BuildContext context) {
-//     height = MediaQuery.sizeOf(context).height;
-//     width = MediaQuery.sizeOf(context).width;
-//   }
-// }
+  static init(BuildContext context) {
+    height = MediaQuery.sizeOf(context).height;
+    width = MediaQuery.sizeOf(context).width;
+  }
+}
